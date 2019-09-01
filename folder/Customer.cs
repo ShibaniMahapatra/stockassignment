2d742a9a61aafb4d62d5b7329a3a9750ed7e4735
Shibani Mahapatra <34315569+ShibaniMahapatra@users.noreply.github.com>
2019-09-01 14:56:04 +0530

    Add files via upload

---
+++ folder/Customer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Customer
+    {
+
+        public void ViewProducts(List<Product> products)
+        {
+            foreach(var product in products)
+            {
+                Console.WriteLine(product.Itemid+" "+product.Item+" "+product.Instock+" "+product.Price);
+            }
+        }
+
+        public void BuyProduct(List<Product> products, List<Product> cartproducts, int itemidtobuy)
+        {
+            //Console.WriteLine("What do you want to buy");
+            //var itemidtobuy = int.Parse(Console.ReadLine());
+            var index = products.IndexOf(products.Find(item => item.Itemid == itemidtobuy));
+            if (index == -1)
+                Console.WriteLine("Not found");
+            else
+            {
+                Console.WriteLine("Index"+index);
+                Console.WriteLine("Enter Quantity");
+                var quantity = int.Parse(Console.ReadLine());
+                if (products[index].Instock < quantity)
+                {
+                    Console.WriteLine("stock not present");
+                }
+                else
+                {
+                    cartproducts.Add(new Product()
+                    {
+                        Itemid = index,
+                        Item = products[index].Item,
+                        Quantity = quantity,
+                        Sum = quantity * products[index].Price,
+                        Price = products[index].Price
+                    });
+                    products[index].Instock = products[index].Instock - quantity;
+                    Console.WriteLine("Product added");
+                }
+            }
+        }
+        public void ViewCart(List<Product> cartproducts)
+        {
+            Console.WriteLine("ItemID \t" + "Quantity\t " + "Price\t" + "Sum");
+            foreach (var product in cartproducts)
+            {
+                Console.WriteLine(product.Itemid + " \t" + product.Item + " " + product.Quantity + " " + product.Price + " " + product.Sum);
+            }
+        }
+    }
+}
---
+++ folder/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Product
+    {
+        public int Itemid { get; set; }
+        public string Item { get; set; }
+        public double Price { get; set; }
+        public int Instock { get; set; }
+        public int Quantity { get; set; }
+        public double Sum { get; set; }
+    }
+}
+
+
+
+
+//productlist.Add({itemid=Constants.Itemid++, item="pen", instock=10, price=20.0});
+//new Product{itemid=Constants.Itemid++, item="book",instock=20,price=25.0},
+//new Product{itemid=Constants.Itemid++, item="pencil",instock=15,price=30.0},
+//new Product{itemid=Constants.Itemid++, item="toys",instock=12,price=10.0}
+
