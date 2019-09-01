a42b1ec30b9b1b3d5943de0502d4634a3e7863e8
Shibani Mahapatra <34315569+ShibaniMahapatra@users.noreply.github.com>
2019-09-01 14:08:56 +0530

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Admin
+    {
+        public void Addnewitem(List<Product> products)
+        {
+
+            Console.WriteLine("Enter item name");
+            var itemname = Console.ReadLine();
+            Console.WriteLine("Enter price");
+            var price = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter quantity");
+            var quantity = int.Parse(Console.ReadLine());
+            products.Add(new Product()
+            {
+                Itemid = Constants.Itemid++,
+                Item = itemname,
+                Price = price,
+                Instock = quantity,
+            }) ;
+            Console.WriteLine("Item Added");
+            Console.ReadKey();
+        }
+        public void Addstock(List<Product>products)
+        {
+            Console.WriteLine("Select itemid to add stock");
+            var itemid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter quantity");
+            int quantitytobeadded = int.Parse(Console.ReadLine());
+            products[itemid].Instock = products[itemid].Instock + quantitytobeadded;
+            ViewProducts(products);
+            Console.ReadKey();
+        }
+        public void ViewProducts(List<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Itemid + " " + product.Item + " " + product.Instock + " " + product.Price);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Admin admin = new Admin();
+            List<Product> productlist = new List<Product>
+            {
+                new Product(){Itemid=Constants.Itemid++, Item="pen", Instock=10, Price=20.0},
+                new Product(){Itemid=Constants.Itemid++, Item="pencil",Instock=15,Price=30.0 },
+                new Product(){Itemid=Constants.Itemid++, Item="book",Instock=20,Price=25.0},
+            };
+            List<Product> cartlist = new List<Product>();
+            while(true)
+            {
+                Console.WriteLine(("Enter 0 for login as admin or 1 for login as customer"));
+                var choice = int.Parse(Console.ReadLine());
+                if (choice == 0)
+                {
+                    Console.WriteLine("Enter 1 for Add new item 2 for update existing stock 3 for display item list 4 for logout");
+                    var adminchoice = int.Parse(Console.ReadLine());
+                    if (adminchoice == 1)
+                        admin.Addnewitem(productlist);
+                    else if(adminchoice==2)
+                    {
+                        admin.Addstock(productlist);
+                    }
+                    else if(adminchoice==3)
+                    {
+                        admin.ViewProducts(productlist);
+                    }
+                }
+                else
+                {
+                    while(true)
+                    {
+
+                        customer.ViewProducts(productlist);
+                        Console.WriteLine("Press -1 to view cart");
+                        Console.WriteLine("What do you want to buy");
+                        int choicetovieworbuy = int.Parse(Console.ReadLine());
+                        if (choicetovieworbuy == -1)
+                            customer.ViewCart(cartlist);
+                        else
+                        {
+                            customer.BuyProduct(productlist, cartlist,choicetovieworbuy);
+                        }
+                        //customer.ViewProducts(productlist);
+                        Console.ReadKey();
+                    }
+                }
+           
+            }
+        }
+    }
+}
+    
+
+
